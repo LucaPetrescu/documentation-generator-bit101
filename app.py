b6f6cddb53e739137f74514e7219997ee2c09da3
LucaPetrescu <luca@botag.no>
2024-04-13 22:25:53 +0300

    fix main

--- app.py
+++ app.py
@@ -16,5 +16,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    print(os.environ['FLASK_RUN_PORT'])
-    app.run(debug=True, host=0.0.0.0, port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
