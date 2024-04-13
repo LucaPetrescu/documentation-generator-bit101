2f02ed50bbd2fbf4befdfe7dee320d1b6fe2725f
DavidNicolae <davidnicolae5858@gmail.com>
2024-04-13 17:45:25 +0300

    Fixed routes bug

--- routes/routes.py
+++ routes/routes.py
@@ -5,6 +5,7 @@ from schemas import ResponseSchema
 from models.responses import ResponseModel
 from docgen import Loader
 from db import db
+from pprint import pprint
 
 blp = Blueprint('back-end', __name__)
 
@@ -12,15 +13,15 @@ blp = Blueprint('back-end', __name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@blp.route('/ask/<string:type>', methods=['POST'])
-def ask(type):
+@blp.route('/askdoc/', methods=['POST'])
+def askdoc():
     payload = request.get_json()
     errors = ResponseSchema().validate(payload)
     if errors:
         abort(400, str(errors))
     
     conversation = ResponseModel.query.filter(and_(ResponseModel.message == payload['message'],
-                                                   ResponseModel.response_type == type)).first()
+                                                   ResponseModel.response_type == 'doc')).first()
     if conversation is None:
         loader = Loader(payload['message'])
 
@@ -28,21 +29,21 @@ def ask(type):
         documentation = loader.get_response_doc()
         conversation = ResponseModel(message=payload['message'],
                                  response=documentation,
-                                 response_type=type)
+                                 response_type='doc')
         db.session.add(conversation)
         db.session.commit()
 
     return ResponseSchema().dump(conversation), 200
 
-@blp.route('/ask/<string:type>', methods=['POST'])
-def ask(type):
+@blp.route('/askupdate', methods=['POST'])
+def askupdate():
     payload = request.get_json()
     errors = ResponseSchema().validate(payload)
     if errors:
         abort(400, str(errors))
     
     conversation = ResponseModel.query.filter(and_(ResponseModel.message == payload['message'],
-                                                   ResponseModel.response_type == type)).first()
+                                                   ResponseModel.response_type == 'update')).first()
     if conversation is None:
         loader = Loader(payload['message'])
 
@@ -50,7 +51,7 @@ def ask(type):
         documentation = loader.get_response_update()
         conversation = ResponseModel(message=payload['message'],
                                  response=documentation,
-                                 response_type=type)
+                                 response_type='update')
         db.session.add(conversation)
         db.session.commit()
 
